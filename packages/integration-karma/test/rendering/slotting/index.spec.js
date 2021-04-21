4917013a5b08beb5d7192a8e0a9de34108bedbbd
Nolan Lawson <nlawson@salesforce.com>
2021-04-20 17:09:26 -0700

    Revert "tmp: remove stuff I do not care about"
    This reverts commit 83b891e91ec399b44508ac01bc1bd63c4672ff5f.

---
+++ packages/integration-karma/test/rendering/slotting/index.spec.js
@@ -0,0 +1,65 @@
+import { createElement } from 'lwc';
+
+import RenderCountParent from 'x/renderCountParent';
+import FallbackContentReuseParent from 'x/fallbackContentReuseParent';
+import RegressionContainer from 'x/regressionContainer';
+
+// TODO [#1617]: Engine currently has trouble with slotting and invocation of the renderedCallback.
+xit('should not render if the slotted content changes', () => {
+    const elm = createElement('x-render-count-parent', { is: RenderCountParent });
+    elm.value = 'initial';
+    document.body.appendChild(elm);
+
+    expect(elm.getRenderCount()).toBe(1);
+    expect(elm.shadowRoot.querySelector('x-render-count-child').getRenderCount()).toBe(1);
+    expect(elm.shadowRoot.querySelector('div').textContent).toBe('initial');
+
+    elm.value = 'updated';
+
+    return Promise.resolve().then(() => {
+        expect(elm.getRenderCount()).toBe(2);
+        expect(elm.shadowRoot.querySelector('x-render-count-child').getRenderCount()).toBe(1);
+        expect(elm.shadowRoot.querySelector('div').textContent).toBe('updated');
+    });
+});
+
+it('#663 - should not reuse elements from the fallback slot content', () => {
+    const elm = createElement('x-fallback-content-reuse-parent', {
+        is: FallbackContentReuseParent,
+    });
+    document.body.appendChild(elm);
+
+    expect(elm.shadowRoot.querySelector('x-fallback-content-reuse-child').innerHTML).toBe('');
+    elm.renderSlotted = true;
+
+    return Promise.resolve().then(() => {
+        expect(elm.shadowRoot.querySelector('x-fallback-content-reuse-child').innerHTML).toBe(
+            '<div>Default slotted</div><div slot="foo">Named slotted</div>'
+        );
+    });
+});
+
+it('should not throw error when updating slotted content triggers next tick re-render in component receiving the slotted content', (done) => {
+    // Regression introduced in #1617 refactor(engine): improving the diffing algo for slotted elements
+    const elm = createElement('x-regression-container', {
+        is: RegressionContainer,
+    });
+    document.body.appendChild(elm);
+
+    requestAnimationFrame(() => {
+        const results = elm.shadowRoot.querySelectorAll('.text-result');
+
+        expect(results.length).toBe(1);
+        expect(results[0].textContent).toBe('Inner visible truetoggle');
+        elm.shadowRoot.querySelector('button').click();
+
+        requestAnimationFrame(() => {
+            const results = elm.shadowRoot.querySelectorAll('.text-result');
+
+            expect(results.length).toBe(1);
+            expect(results[0].textContent).toBe('Inner visible falsetoggle');
+
+            done();
+        });
+    });
+});
