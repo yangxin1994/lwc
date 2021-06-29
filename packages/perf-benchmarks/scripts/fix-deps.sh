5e28246f4f883d919378cd99580114f2a6cd91fd
Nolan Lawson <nlawson@salesforce.com>
2021-06-29 16:44:26 -0700

    fix: fix license header

--- packages/perf-benchmarks/scripts/fix-deps.sh
+++ packages/perf-benchmarks/scripts/fix-deps.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#
+# Copyright (c) 2018, salesforce.com, inc.
+# All rights reserved.
+# SPDX-License-Identifier: MIT
+# For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+#
 # Create symlinks in the local node_modules directory to point to the root ones.
 # This is bizarre, but it seems to be the only way to get Tachometer to recognize
 # our local deps, e.g. when running in `this-patch` mode (not `tip-of-tree` mode).
